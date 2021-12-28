5a1281b79bde845762431172ea4ca86f5dd8c4fe
Benjamín de Jesús Pérez Aguilar <benjaminperez201831504@cunoc.edu.gt>
2021-12-28 17:11:02 -0600

    la contraseña

--- api/module/coneccion.js
+++ api/module/coneccion.js
@@ -1,9 +1,9 @@
 /**** mysql */
 //variables de entorno, esas vienen de docker
-const mysqlhost ='192.168.1.9';
-const mysqluser =  "prueba";
-const mysqlpass = "RNnxW9hp(&U~x2s";
-const mysqldatabase = "laboratorio_clinico";
+const mysqlhost ='localhost';
+const mysqluser =  "clinicaMedicadmin";
+const mysqlpass = "Password123$";
+const mysqldatabase = "clinicaMedica";
 //paquete
 var mysql = require('mysql2');
 //conexión
