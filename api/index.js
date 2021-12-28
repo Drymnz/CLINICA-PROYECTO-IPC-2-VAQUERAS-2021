a589c8ef71b53abd2a8bb93357a47c5516799888
David Barrera <david1315barrera@gmail.com>
2021-12-28 01:46:31 -0600

    subiendo vistas

--- api/index.js
+++ api/index.js
@@ -65,6 +65,79 @@ app.get('/Resultados' , (req , res)=>{
     console.log(path.join(__dirname,'../views/VerConsulta.html'));
     res.status(201).sendFile(path.join(__dirname,'../views/VerConsulta.html'));
 });
+
+//----VISTAS LLAMADAS DESDE DEPENDIENDO DEL LOGIN
+//-----share/Clientes
+//-----share/admin
+//-----share/laboratorista
+ //----------------------------Secretaria----------------------------------------------
+
+ app.get('/Pacientes' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/IngresoClientes.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/IngresoClientes.html'));
+});
+app.get('/Clientes' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/share/Clientes.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/share/Clientes.html'));
+});
+app.get('/Registro' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/Registro.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/Registro.html'));
+});
+app.get('/RegistroSec' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/secretaria/RegistroSec.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/secretaria/RegistroSec.html'));
+});
+app.get('/Consultas' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/secretaria/consultasSec.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/secretaria/consultasSec.html'));
+});
+/*app.get('/Agendarcita' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/secretaria/agendarcita.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/secretaria/agendarcita.html'));
+});*/
+app.get('/Factura' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/secretaria/Factura.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/secretaria/Factura.html'));
+});
+ app.listen(port , ()=> console.log('> Server is up and running on port : ' + port))
+
+ //---------------------------------------------------------- Admin
+
+ app.get('/Inventario' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/Admin/Inventario.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/Admin/Inventario.html'));
+});
+app.get('/registrarEmpleado' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/Admin/registrarEmpleado.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/Admin/registrarEmpleado.html'));
+});
+app.get('/registrarExamen' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/Admin/registrarExamen.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/Admin/registrarExamen.html'));
+});
+app.get('/Admin' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/share/Admin.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/share/Admin.html'));
+});
+ //---------------------------------------------------------- Laboratorista
+ app.get('/laboratorista' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/share/laboratorista.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/share/laboratorista.html'));
+});
+
+app.get('/Resutados' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/lab/Resutados.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/lab/Resutados.html'));
+});
+
+app.get('/vercitas' , (req , res)=>{
+    console.log(path.join(__dirname,'../views/lab/vercitas.html'));
+    res.status(201).sendFile(path.join(__dirname,'../views/lab/vercitas.html'));
+});
+
+
+
 // manejo de errores
 app.use((error , req , res , next)=>{
     res.status(400).json({
